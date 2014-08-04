7ce5cf31b4a9f2171ea330b9c84f7940d8858d0b
taoyuan <torworx@gmail.com>
2014-08-04 09:40:05 +0800

    Initial commit

---
+++ test/veritoken.test.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var _ = require('lodash');
+var t = require('chai').assert;
+var express = require('express');
+var request = require('supertest');
+var veritoken = require('../');
+
+
+describe('veritoken', function () {
+
+    it('should verify token', function (done) {
+        var user = {name: 'ty', token: '1234567890abcdefgh'};
+        var app = express();
+        app.use(veritoken({
+            headers: ['authorization'],
+            attr: 'user'
+        }, function (token, cb) {
+            if (token === user.token) {
+                cb(null, user);
+            } else {
+                cb();
+            }
+        }));
+        app.use(function (req, res, next) {
+            t.equal(req.user, user);
+            res.status(200).end();
+        });
+
+        request(app)
+            .get('/')
+            .set('authorization', user.token)
+            .expect(200)
+            .end(function (err, res) {
+                t.notOk(res.body.error);
+                done();
+            });
+    });
+});
